518da153e3ce8b9062d777244d20fb270a62d775
Raj <vishwakarmaraj070@gmail.com>
2019-08-23 18:19:50 +0530

    final

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default class App extends Component {
           <div className="resume-wrapper-inner mx-auto text-left bg-white shadow-lg">
             <header className="resume-header pt-4 pt-md-0">
               <div className="media flex-column flex-md-row">
-                <img className="mr-3 img-fluid picture mx-auto" src={require('./assets/profile.jpg')} alt="Profile" />
+                <img className="mr-3 img-fluid picture mx-auto" src={require('./profile.jpg')} alt="Profile" />
                 <div className="media-body p-4 d-flex flex-column flex-md-row mx-auto mx-lg-0">
                   <div className="primary-info">
                     <h1 className="name mt-0 mb-1 text-white text-uppercase text-uppercase">Raj Vishwakarma</h1>
@@ -351,7 +351,7 @@ export default class App extends Component {
                      <p>Interest to see my all project go to my <a href="https://rajv-portfolio.herokuapp.com/" target="_blank">Portfolio website</a></p>
                     </div>
                   </section>
-                  <a class="btn theme-btn-on-bg download-resume position-absolute font-weight-bold mx-auto" href="http://webhut.co/wp-content/uploads/2019/08/resume.pdf" download target="_blank"><svg class="svg-inline--fa fa-download fa-w-16 mr-2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="download" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"></path></svg>Download PDF Version</a>
+                  <a className="btn theme-btn-on-bg download-resume position-absolute font-weight-bold mx-auto" href="http://webhut.co/wp-content/uploads/2019/08/resume.pdf" download target="_blank"><svg className="svg-inline--fa fa-download fa-w-16 mr-2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="download" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"></path></svg>Download PDF Version</a>
                 </div>
               </div>
             </div>
